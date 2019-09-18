82d9fb19417f85b7e322deaf8d027f3d0a8a8dfb
Quang <tienquang98tb@gmail.com>
2019-09-18 16:36:20 +0700

    create suppliers and fix model

---
+++ common/models/suppliers.js
@@ -0,0 +1,175 @@
+let to = require('await-to-js').to;
+
+'use_strict';
+
+module.exports = function(Supplier) {
+  const Promise = require('bluebird')
+	  //create Supplier
+	  Supplier.createSupplier = async function(
+        uid, 
+        name, 
+        email, 
+        address,
+        phone) {
+
+        const SupplierData = {
+            uid: uid,
+            name: name,
+            email: email,
+            address: address,
+            phone: phone,
+            createdAt: new Date(),
+            enable: 1;
+        }
+        try {
+            const data = await Supplier.create(SupplierData)
+            return data
+          } catch (err) {
+            console.log('create Supplier', err)
+            throw err
+          }
+        }
+    
+    //read Supplier
+    Supplier.readSupplier = async function(id) {
+        try {
+            const data = await Supplier.findById(id, {
+                where: {
+                enable: 1
+                }
+            });
+            return data;
+        } catch (err) {
+            console.log('read Supplier', err)
+            throw err
+        }
+    }
+
+    //update Supplier
+    Supplier.updateSupplier = async function(
+        id, 
+        name, 
+        email, 
+        address,
+        phone) {
+    	
+        const SupplierData = {
+            name: name,
+            email: email,
+            address: address,
+            phone: phone,
+            updatedAt: new Date(),
+        }
+
+        try {
+            const data = await Supplier.upsertWithWhere(
+              {
+                id: Supplier.id
+              },
+              SupplierData
+            )
+            return data
+          } catch (err) {
+            console.log('update Supplier', err)
+            throw err
+          }
+    }
+
+    //delete Supplier 
+    Supplier.deleteSupplier = async function(id) {
+        let [err, supplier] = await to(Supplier.findOne({where: {id: id}}))
+        if (supplier == null) {
+            return [200, 'can not find supplier']
+        }
+        Supplier.destroyById(supplier.id)
+        return [200, 'delete supplier sucess']
+    }
+
+    // list Suppliers paganation(4)
+    Supplier.listSupplier = async function(page, pageSize) {
+        try {
+          const [data, total] = await Promise.all([
+            Supplier.find({
+              where: {
+                enable: 1
+              },
+              fields: {
+                name: true
+              }
+            }),
+            Supplier.count({
+              enable: 1
+            })
+          ])
+
+          return {
+            rows: data,
+            page: page,
+            pageSize: pageSize,
+            total: total
+          }
+        } catch (err) {
+          console.log('list Supplier', err)
+          throw err
+        }
+    }
+    
+    Supplier.remoteMethod('createSupplier', 
+      {
+        http: {path: '/create', verb: 'post'},
+        accepts: [
+          {arg: 'uid', type: 'number', required: true},
+          {arg: 'name', type: 'string', required: true},
+          {arg: 'email', type: 'string', required: true},
+          {arg: 'address', type: 'string', required: false},
+          {arg: 'phone', type: 'boolean', required: false},
+        ],
+        returns: { arg: 'data' },
+      }
+    )
+
+    Supplier.remoteMethod('readSupplier', 
+      {
+        http: {path: '/read', verb: 'post'},
+        accepts: [
+            {arg: 'id', type: 'number', required: true}],
+        returns: { arg: 'data' }
+      },
+    )
+
+    Supplier.remoteMethod('updateSupplier', 
+      {
+        http: {path: '/update', verb: 'post'},
+        accepts: [
+          {arg: 'id', type: 'number', required: true},
+          {arg: 'name', type: 'string', required: true},
+          {arg: 'email', type: 'string', required: true},
+          {arg: 'address', type: 'string', required: false},
+          {arg: 'phone', type: 'boolean', required: false},
+        ],
+        returns: { arg: 'data' }
+      },
+    )
+
+    Supplier.remoteMethod('deleteSupplier', 
+      {
+        http: {path: '/delete', verb: 'delete'},
+        accepts: [
+            {arg: 'id', type: 'number', required: true}
+        ],
+        returns: [
+            {arg: 'status', type: 'number'},
+            {arg: 'message', type: 'string'}]
+      }
+    )
+
+    Supplier.remoteMethod('listSupplier', 
+      {
+        http: {verb: 'post', path: '/list' },
+        accepts: [
+          { arg: 'page', type: 'number', default: '0'},
+          { arg: 'pageSize', type: 'number', default: '4'}],
+        returns: { arg: 'data' },
+      }
+    )
+};
